6f62d8bfc4ba2231197b0cd3a0d1935d09f6a810
anonphin <anonphin.pro@gmail.com>
2022-08-31 22:56:11 +0700

    new

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,8 +1,12 @@
 @extends('frontend.master')
 
-@section('title',"$setting->meta_title")
+@section('title',"home")
+@section('meta_description',"home page")
+@section('meta_keyword',"home page keywords")
+
+{{-- @section('title',"$setting->meta_title")
 @section('meta_description',"$setting->meta_description")
-@section('meta_keyword',"$setting->meta_keyword")
+@section('meta_keyword',"$setting->meta_keyword") --}}
 
 @section('content')
 
